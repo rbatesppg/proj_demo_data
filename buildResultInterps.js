1bd81ba6445d5b03ac2e36681df112e996a7d557
DESKTOP-J0O52AL\Reilly Bates <rbates@pawprintgenetics.com>
2020-09-11 16:49:18 -0700

    adjusting so all fields ResultInterps are populated even if undefined in database

--- buildResultInterps.js
+++ buildResultInterps.js
@@ -18,27 +18,55 @@ let animal;
 for(resultInterp of resultInterps){
     let newEntry = {
         caseId: '',
+        animalId: '',
+        callName: '',
         mutationIds: {},
         mutationGroupId: '',
         platform: {},
         breed: '',
+        sex: '',
         date: '',
         genotype: '',
         isPublished: ''
     };
-    newEntry.caseId = resultInterp._source.caseId;
-    newEntry.mutationIds = resultInterp._source.mutationIds;
-    newEntry.mutationGroupId = resultInterp._source.mutationGroupId;
-    newEntry.date = resultInterp._source.meta.created;
-    newEntry.genotype = resultInterp._source.genotype;
-    newEntry.isPublished = resultInterp._source.isPublished;
+    if(resultInterp._source.caseId !== undefined){
+        newEntry.caseId = resultInterp._source.caseId;
+    }
+    if(resultInterp._source.mutationIds !== undefined){
+        newEntry.mutationIds = resultInterp._source.mutationIds;
+    }
+    if(resultInterp._source.mutationGroupId !== undefined){
+        newEntry.mutationGroupId = resultInterp._source.mutationGroupId;
+    }
+    if(resultInterp._source.meta.created !== undefined){
+        newEntry.date = resultInterp._source.meta.created;
+    }
+    if(resultInterp._source.genotype !== undefined){
+        newEntry.genotype = resultInterp._source.genotype;
+    }
+    if(resultInterp._source.isPublished !== undefined){
+        newEntry.isPublished = resultInterp._source.isPublished;
+    }
 
     // getting breed and platform from animal Index record, by caseId
     let matchAnimal = false;
     for(animal of animalsList){
         if(animal.caseId === newEntry.caseId){
-            newEntry.platform = animal.platform;
-            newEntry.breed = animal.breed;
+            if(animal.platform !== undefined){
+                newEntry.platform = animal.platform;
+            }
+            if(animal.breed !== undefined){
+                newEntry.breed = animal.breed;
+            }
+            if(animal.animalId !== undefined){
+                newEntry.animalId = animal.animalId;
+            }
+            if(animal.callName !== undefined){
+                newEntry.callName = animal.callName;
+            }
+            if(animal.sex !== undefined){
+                newEntry.sex = animal.sex;
+            }
             matchAnimal = true;
         }
     }
